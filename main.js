6243c5cc6edd2421cba5470472635cebd99329c3
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-30 15:09:38 +0200

    Creato array con 10 mail

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ const app = new Vue ({
         mail: ``,
         mailList: [],
     },
+    mounted() {
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then((elm) => {
+            for (let i = 0; i < 10; i++) {
+                this.mail = elm.data.response
+                this.mailList.push(this.mail)
+            }
+            console.log(this.mailList)
+        })
+    }
 })
