e113d4dba70ad1539579182801b92684fed94a4c
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-30 16:04:12 +0200

    Aggiornato ciclo for

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const app = new Vue ({
     el: `#app`,
     data: {
-        mail: ``,
         mailList: [],
     },
-    mounted() {
-        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        .then((elm) => {
-            for (let i = 0; i < 10; i++) {
-                this.mail = elm.data.response
-                this.mailList.push(this.mail)
-            }
-            console.log(this.mailList)
-        })
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then((elm) => {
+                console.log(elm.data.response)
+                this.mailList.push(elm.data.response)
+            })
+        }
     }
 })
